76857393ca4a57293d2fd1315caa5376808d79fc
ckchamorro <ckarem@gmail.com>
2026-06-29 21:41:23 -0300

    Actualizacion 2026-06-29 21:41

--- sw.js
+++ sw.js
@@ -1,39 +1,8 @@
-const CACHE = 'training-log-v15';
-const ASSETS = [
-  '/traininglog/',
-  '/traininglog/index.html',
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c => Promise.allSettled(ASSETS.map(a => c.add(a).catch(() => {}))))
-      .then(() => self.skipWaiting())
-  );
-});
-
+// Service worker desactivado temporalmente
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('message', e => {
-  if (e.data === 'SKIP_WAITING') self.skipWaiting();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  if (e.request.url.includes('firestore') || e.request.url.includes('firebase')) return;
-  if (e.request.url.includes('lemonsqueezy') || e.request.url.includes('googleapis')) return;
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      if (res && res.ok) {
-        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-      }
-      return res;
-    }).catch(() => caches.match('/traininglog/')))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   );
 });
